03a10f6c0a322b8865cf80f2e506cbfd28556b9d
Jaehyun Kim <avenj.dev@gmail.com>
2024-06-15 22:34:02 +0900

    feat: Add CSS build script for generating theme CSS variables

---
+++ packages/themes/scripts/build-css-module.js
@@ -0,0 +1,45 @@
+import * as themes from '../dist/index.js';
+import fs from 'fs';
+
+const toCssString = (str) => {
+  return str
+    .replace(/([a-z])(\d)/, '$1-$2')
+    .replace(/([A-Z])/g, '-$1')
+    .toLowerCase();
+};
+
+const generateThemeCSSVariables = () => {
+  const cssString = [];
+
+  Object.entries(themes.vars).forEach(([key, value]) => {
+    if (key === 'colors') {
+      Object.entries(value.$static).forEach(([theme, themeColor]) => {
+        if (theme === 'light') {
+          const selector = ':root';
+
+          const cssVariables = Object.entries(themeColor)
+            .map(([color, scales]) =>
+              Object.entries(scales)
+                .map(
+                  ([scale, value]) =>
+                    `  --${theme}-${toCssString(color)}-${scale}: ${value};`
+                )
+                .join('\n')
+            )
+            .join('\n\n');
+
+          cssString.push(`${selector} {\n${cssVariables}\n}`);
+        }
+      });
+    }
+  });
+
+  return cssString.join('\n');
+};
+
+const generateThemeCSS = () => {
+  const variables = generateThemeCSSVariables();
+  fs.writeFileSync('dist/themes.css', variables);
+};
+
+generateThemeCSS();
