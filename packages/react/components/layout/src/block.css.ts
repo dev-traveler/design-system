a3c04babf06337610748a3bcfe4c05c76e9eab60
Jaehyun Kim <avenj.dev@gmail.com>
2024-06-20 14:50:24 +0900

    feat: Add storybook to packages

--- packages/react/components/layout/src/block.css.ts
+++ packages/react/components/layout/src/block.css.ts
@@ -4,5 +4,5 @@ import { style } from "@vanilla-extract/css";
 export const blockStyle = style({
   width: "100%",
   height: "100%",
-  backgroundColor: vars.colors.$scale.blue[500],
+  backgroundColor: vars.colors.$scale.purple[500],
 });
---
+++ services/storybook/stories/ReactComponents/Layout/Block.stories.tsx
@@ -0,0 +1,13 @@
+import "@dev-traveler/react-components-layout/styles.css";
+import { Block as _Block } from "@dev-traveler/react-components-layout";
+
+export default {
+  title: "React Components/Layout/Block",
+  component: _Block,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export const BlockStory = {};
