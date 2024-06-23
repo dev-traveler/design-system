7a98361f4471e3ded48bd2ef7706c58fcc652771
Jaehyun Kim <avenj.dev@gmail.com>
2024-06-23 18:09:53 +0900

    feat: Remove unused Block component and related files

--- packages/react/components/layout/src/block.css.ts
+++
@@ -1,8 +0,0 @@
-import { vars } from "@dev-traveler/themes";
-import { style } from "@vanilla-extract/css";
-
-export const blockStyle = style({
-  width: "100px",
-  height: "100px",
-  backgroundColor: vars.colors.$scale.blue[500],
-});
--- packages/react/components/layout/src/index.ts
+++ packages/react/components/layout/src/index.ts
@@ -1,3 +1,2 @@
-export { Block } from "./Block";
 export { Box } from "./layout";
 export type { BoxProps } from "./layout";
