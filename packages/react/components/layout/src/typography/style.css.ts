dfb52cccc9c6c7efe995c9bb0f1ee393ad456f9a
Jaehyun Kim <avenj.dev@gmail.com>
2024-06-23 18:46:55 +0900

    feat: Add Text component to react components layout package

---
+++ packages/react/components/layout/src/typography/style.css.ts
@@ -0,0 +1,13 @@
+import { classes } from "@dev-traveler/themes";
+import { recipe } from "@vanilla-extract/recipes";
+
+export const textStyle = recipe({
+  variants: {
+    fontSize: {
+      ...classes.typography.text,
+    },
+    defaultVariants: {
+      fontSize: "xl",
+    },
+  },
+});
---
+++ services/storybook/stories/ReactComponents/Layout/Text.stories.tsx
@@ -0,0 +1,31 @@
+import "@dev-traveler/react-components-layout/styles.css";
+import { classes, vars } from "@dev-traveler/themes";
+import { Text as _Text } from "@dev-traveler/react-components-layout";
+
+export default {
+  title: "React Components/Layout/Text ",
+  component: _Text,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    fontSize: {
+      options: Object.keys(classes.typography.text),
+      control: { type: "select" },
+    },
+    color: {
+      options: Object.keys(vars.colors.$scale),
+      control: { type: "select" },
+    },
+  },
+};
+
+export const TextStory = {
+  args: {
+    as: "p",
+    fontSize: "xl",
+    children: "Hello, World",
+    color: "gray",
+  },
+};
